c3f2c0349e9db30d845b57eee867e770883ed7e4
Lucas Kurata <lucas.kurata@hotmail.com>
2021-06-26 15:06:35 -0300

    Implementa Estruturas Condicionais Java

---
+++ Modulo_Java/Aula02-EstruturaCondicional/src/DesvioSimples.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DesvioSimples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Uma ONG criou um jogo para alertar as pessoas sobre os riscos das drogas.
+		 *Por se tratar de um assunto sensível, ficou decidido que a idade mínima para jogar é de 12 anos.
+		 *Sua função é criar um programa que receba a idade do usuário e exiba a mensagem "Você pode jogar" caso ele tenha 12 anos ou mais. */
+		
+		Scanner leitor = new Scanner(System.in);
+		int idade;
+		System.out.println("Por favor, digite a sua idade!");
+		idade = leitor.nextInt();
+		System.out.println("Deixe-me verificar sua idade...");
+		
+		//A baixo, temos um if simples apenas quando o resultado do teste é verdadeiro
+		if (idade >= 12) {
+			System.out.println("Você poderá jogar por ter no mínimo 12 anos!");
+		}
+		
+		leitor.close();
+	}
+
+}
